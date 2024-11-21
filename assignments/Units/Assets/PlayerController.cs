85521f283c2678ea69a561cb2838c22d17d91a12
iceage2114 <31936203+iceage2114@users.noreply.github.com>
2024-11-21 18:46:54 -0500

    11/21 6:46

---
+++ assignments/Units/Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody rb;
+    private Vector3 movement;
+    public Camera mainCamera;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal"); // A & D
+        float verticalInput = Input.GetAxisRaw("Vertical");     // W & S
+
+        movement = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = movement * moveSpeed;
+    }
+}
